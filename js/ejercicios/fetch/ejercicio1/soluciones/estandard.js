d2befcb1df16a05e958c194c8e2dd6a6a380e2eb
Lolo <lologameprogrammer@gmail.com>
2024-02-28 14:56:59 +0100

    Agregando sección de Fetch, utilizamos la Fake Json Api

---
+++ js/ejercicios/fetch/ejercicio1/soluciones/estandard.js
@@ -0,0 +1,19 @@
+import { USER_ID, fetchingData } from "./api/api.js";
+
+const allPosts = await fetchingData('users')
+const postsByUserId = async (id) => await fetchingData(`posts?userId=${id}`)
+
+const getImportantInfo = async (posts) => {
+    return posts?.map(({ id, userId, title }) => {
+        return {
+            id: id,
+            userId: userId,
+            postTitle: title,
+            key: Math.floor(Math.random() * 100)
+        }
+    })
+}
+
+const cleanPosts = await getImportantInfo(await postsByUserId(USER_ID))
+
+console.log(cleanPosts)
