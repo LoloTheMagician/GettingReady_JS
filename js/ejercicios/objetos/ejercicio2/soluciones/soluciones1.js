892b72c7ef5c6392dfce4e2b69392f2b171e992a
Lolo <lologameprogrammer@gmail.com>
2024-02-28 13:27:58 +0100

    Solución para el ejercicio del usuario con mayor edad, explicación detallada acompañada

--- js/ejercicios/objetos/ejercicio2/soluciones/soluciones1.js
+++ js/ejercicios/objetos/ejercicio2/soluciones/soluciones1.js
@@ -1,14 +1,24 @@
-import { usuarios } from "../../../../data/users";
+import { usuarios } from "../../../../data/users.js"
 
 const getUserMayorEdad = (usuarios) => {
-    return usuarios.sort((a,b) => {
-        const {edad : edadPrimer} = a
-        const {edad : edadSegundo} = b
-        return edadPrimer - edadSegundo
-    })
+    return usuarios.sort((usuarioDespues, usuarioAntes) => {
+        // console.log('Antes ' , usuarioAntes)
+        // console.log('Despues ', usuarioDespues)
+        // Desempaquetamos el array de edades 
+        const [edadDespues, edadAntes] = [usuarioDespues.edad, usuarioAntes.edad]
+        return edadAntes - edadDespues
+    }).at(0)
 }
 
 console.log(getUserMayorEdad(usuarios))
-const getNombre = () => {
 
-}
+const getNombre = ({ name }) => name
+
+// Las funciones en JS son de primera clase (First Class Citizens) 
+// Se pueden tratar como valores --> se pueden asignar, usar como parámetros
+// En este caso asignamos como valor de 'nombreUsuarioMayor' la función 'getNombre'
+// Y la función 'getNombre' le pasamos como parámetro otra función ('getUserMayorEdad')
+// Estamos haciendo un approach similar a la programación funcional (Buscar información al respecto)
+const nombreUsuarioMayor = getNombre(getUserMayorEdad(usuarios))
+
+console.log(nombreUsuarioMayor)
