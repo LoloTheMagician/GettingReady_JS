fb3f245e10bdbbad4b7ef2914404a8ecd75eff89
loloTheMagician <elperiplodelolo@gmail.com>
2024-03-01 09:55:29 +0100

    Agregando comentarios al ejercicio de nombres en mayúscula

--- js/ejercicios/arrays/ejercicio1/soluciones/estandard.js
+++ js/ejercicios/arrays/ejercicio1/soluciones/estandard.js
@@ -2,13 +2,21 @@ import { nombres } from '../../../../data/names.js'
 
 const wordscapitalize = (words = []) => {
     const wordscapitalize = []
+
     for (const word of words) {
+        // Separamos la palabra en dos partes: primer letra (string), resto de la palabra (array)
         const [firstLetter, ...wordWithoutFirstLetter] = word
+
         const firstLetterUpperCase = firstLetter.toUpperCase()
+        // Convertimos el array de letras a una string --> .join(separador = '')
         const wordToString = wordWithoutFirstLetter.join('')
+
         const newWord = firstLetterUpperCase + wordToString
+        // Agregamos la palabra capitalize
         wordscapitalize.push(newWord)
+
     }
+
     return wordscapitalize
 }
 
